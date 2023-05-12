b6ab859a08570994b4fbd6dc195c2609dce17317
JD Tadlock <jt7903@gmail.com>
2023-05-12 14:41:02 -0400

    ready for deploy?

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/drink_app_db');
+const PORT = process.env.PORT;
+
+mongoose.connect(PORT ? process.env.DB_URL : 'mongodb://127.0.0.1:27017/drink_app_db');
 
 module.exports = mongoose.connection;
--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(express.static('../client/dist'));
+app.use(express.static('../client/build'));
 
 app.use(session({
   secret: process.env.SESSION_SECRET,
