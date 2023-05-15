fe910a15e497cb0e36339906446580af6bf37876
JD Tadlock <jt7903@gmail.com>
2023-05-15 11:58:47 -0400

    finished example query with args

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -2,10 +2,14 @@ const Drink = require('../models/Drink');
 
 const resolvers = {
   Query: {
-    async getDrinks() {
+    async getDrinks(_, args, context) {
       const drinks = await Drink.find().populate('user');
 
       return drinks;
+    },
+
+    testQuery(_, args, context) {
+      return args;
     }
   }
 };
